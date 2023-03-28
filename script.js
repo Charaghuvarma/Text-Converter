ebda7c845561c54743602e72bd2f8a22ad7d114e
Charaghuvarma <charaghuvarma9@gmail.com>
2023-03-28 10:39:34 +0530

    updated functions

--- script.js
+++ script.js
@@ -1,24 +1,60 @@
-const form = document.querySelector('form');
-const generatorType = document.getElementById('generator-type');
-const dataInput = document.getElementById('data-input');
+function generateQRCode(data) {
+  const imgbox = document.getElementById('imgbox');
+  const data_input = document.getElementById('data_input');
+  imgbox.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + data_input.value;
+}
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  
-  const selectedGenerator = generatorType.value;
-  const data = dataInput.value;
-  
-  switch (selectedGenerator) {
+function generateBarcode(data_input) {
+  JsBarcode("#barcode", data_input, {
+    format: 'code128',
+    displayValue: true,
+  });
+}
+
+function encryptData(data) {
+  var encodedstring;
+  var basestring = document.getElementById('data_input').value;
+  encodedstring = window.btoa(basestring);
+  document.getElementById('textbox').value = encodedstring;
+  document.getElementById('textbox').style.display = 'block';
+}
+
+function decryptData(data) {
+  var decodedstring;
+  var upstring = document.getElementById('data_input').value;
+  decodedstring = window.atob(upstring);
+  document.getElementById('textbox1').value = decodedstring;
+  document.getElementById('textbox1').style.display = 'block';
+}
+
+function generateData() {
+  const inputData = document.getElementById('data_input').value;
+  if (inputData == '') {
+    alert('Please enter some data before generating.');
+    return false;
+  }
+
+  const generatorType = document.getElementById('generator-type').value;
+  switch (generatorType) {
     case 'qr':
-      // Call QR code generation function
+      generateQRCode(inputData);
       break;
     case 'barcode':
-      // Call barcode generation function
+      generateBarcode(inputData);
       break;
     case 'encryption':
-      // Call encryption function
+      encryptData(inputData);
+      break;
+    case 'decryption':
+      decryptData(inputData);
       break;
     default:
       console.log('Invalid generator type');
   }
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  generateData();
 });
